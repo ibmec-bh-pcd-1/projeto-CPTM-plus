a1786fd1eea268d29aa1fa9979853986f9135161
vnevesl <vineveslima11@gmail.com>
2024-10-28 16:30:24 -0300

    mudança do design do menu hamburguer - vitor neves e joao pedro

--- src/content/scripts/script.js
+++ src/content/scripts/script.js
@@ -1,1 +1,6 @@
-
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.user-icon').addEventListener('click', function() {
+        const menu = document.querySelector('.user-menu');
+        menu.classList.toggle('active');
+    });
+});
