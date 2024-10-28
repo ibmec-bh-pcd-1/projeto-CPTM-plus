90f4f18409596b3b5773d4b53155d71e05b3f272
joao-castro <jpcastrocruzeirotetra2006@gmail.com>
2024-10-28 14:33:00 -0300

    teste menu lateral

--- src/content/scripts/script.js
+++ src/content/scripts/script.js
@@ -1,1 +1,24 @@
+function toggleMenu() {
+    const menu = document.getElementById('side-menu');
+    console.log('Toggle menu called');
+    if (menu.style.right === '0px') {
+        menu.style.right = '-200px'; // Fecha o menu
+    } else {
+        menu.style.right = '0px'; // Abre o menu
+    }
+}
 
+function logout() {
+    alert("Sair da conta");
+    window.location.href = "paglogin.html"
+}
+
+function changeAccount() {
+    alert("Mudar de conta");
+    window.location.href = "paglogin.html"
+}
+
+function accessAccount() {
+    alert("Acessar conta");
+    // Adicione a lógica para acessar a conta aqui
+}
