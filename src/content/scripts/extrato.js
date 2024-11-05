e4f71eae758ce77e3f3829a1ea6dff60573ef54b
vnevesl <vineveslima11@gmail.com>
2024-11-05 09:10:36 -0300

    alterações na logo e no js extrato

--- src/content/scripts/extrato.js
+++ src/content/scripts/extrato.js
@@ -1,10 +1,10 @@
 const transacoes = [
-    { data: "01/10/2024", valor: "R$70,00", detalhes: "Depósito feito no dia 4/11/24." },
-    { data: "05/10/2024", valor: "R$50,00", detalhes: "Depósito feito no dia 9/10/24." },
-    { data: "10/10/2024", valor: "R$30,00", detalhes: "Depósito feito no dia 8/10/24." },
-    { data: "10/10/2024", valor: "R$30,00", detalhes: "Depósito feito no dia 7/10/24." },
-    { data: "10/10/2024", valor: "R$30,00", detalhes: "Depósito feito no dia 6/10/24." },
-    { data: "10/10/2024", valor: "R$30,00", detalhes: "Deposíto feito no dia 5/10/24." },
+    { data: "01/10/2024", tipo: "Compra", valor: "R$70,00", detalhes: "Compra de 14 bilhetes" },
+    { data: "05/10/2024", tipo: "Depósito", valor: "R$50,00", detalhes: "Depósito feito via PIX." },
+    { data: "10/10/2024", tipo: "Depósito", valor: "R$30,00", detalhes: "Depósito feito via CARTÃO." },
+    { data: "7/10/2024", tipo: "Depósito", valor: "R$30,00", detalhes: "Depósito feito via CARTÃO." },
+    { data: "6/10/2024", tipo: "Depósito", valor: "R$30,00", detalhes: "Depósito feito via PIX." },
+    { data: "5/10/2024", tipo: "Depósito", valor: "R$30,00", detalhes: "Deposíto feito via CARTÃO." },
 
 ];
 
@@ -17,7 +17,7 @@ function carregarTransacoes() {
 
         transacaoDiv.innerHTML = `
             <div class="transaction-summary">
-                <p>${transacao.data} - ${transacao.valor}</p>
+                <p>${transacao.data} - ${transacao.tipo} - ${transacao.valor}</p>
                 <button class="toggle-details" onclick="toggleDetalhes(${index})">Detalhes</button>
             </div>
             <div class="transaction-details hidden" id="detalhes-${index}">
