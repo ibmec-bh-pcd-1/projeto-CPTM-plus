3c9b5f84b2d45f2e0a284810ec0adf5965fa581a
vnevesl <vineveslima11@gmail.com>
2024-11-07 18:30:18 -0300

    alterações ouvidoria, js, layout e adicionar icones vitor neves e joão pedro

--- src/content/scripts/ouvidoria.js
+++ src/content/scripts/ouvidoria.js
@@ -11,3 +11,7 @@ function fecharConfirmacao() {
     document.getElementById('confirmacao').classList.add('hidden');
     document.getElementById('ouvidoriaForm').reset();
 }
+
+function goToHome() {
+    window.location.href = 'home.html';
+}
