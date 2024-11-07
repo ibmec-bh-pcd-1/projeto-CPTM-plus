f62d935ee9572dafc9bf8e13654633b0fca36595
vnevesl <vineveslima11@gmail.com>
2024-11-07 18:11:58 -0300

    ouvidoria html, css e js vitor neves e joao pedro

---
+++ src/content/scripts/ouvidoria.js
@@ -0,0 +1,13 @@
+document.getElementById('ouvidoriaForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    document.getElementById('confirmacao').classList.remove('hidden');
+});
+
+document.querySelector('.chatbot-button').addEventListener('click', function() {
+    alert("Chatbot em desenvolvimento.");
+});
+
+function fecharConfirmacao() {
+    document.getElementById('confirmacao').classList.add('hidden');
+    document.getElementById('ouvidoriaForm').reset();
+}
